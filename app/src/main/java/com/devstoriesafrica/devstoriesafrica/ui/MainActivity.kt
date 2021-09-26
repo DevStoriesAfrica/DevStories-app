dcf03a607cd32a420fa3af7a01e6e80e9aff64d0
kibet <kibettheophilus@gmail.com>
2021-09-26 12:08:03 +0300

    Added View Binding

--- app/src/main/java/com/devstoriesafrica/devstoriesafrica/ui/MainActivity.kt
+++ app/src/main/java/com/devstoriesafrica/devstoriesafrica/ui/MainActivity.kt
@@ -3,10 +3,14 @@ package com.devstoriesafrica.devstoriesafrica.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.devstoriesafrica.devstoriesafrica.R
+import com.devstoriesafrica.devstoriesafrica.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setTheme(R.style.Theme_DevStoriesAfrica)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
