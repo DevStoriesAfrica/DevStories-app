f4fb6cc79aff5379b27912aad10f7f0cf936333a
Daniel Waiguru <danielirungu91@gmail.com>
2021-10-06 15:42:06 +0300

    Implement LoginScreen Ui design

--- app/src/main/java/com/devstoriesafrica/devstoriesafrica/ui/auth/fragments/LoginFragment.kt
+++ app/src/main/java/com/devstoriesafrica/devstoriesafrica/ui/auth/fragments/LoginFragment.kt
@@ -1,16 +1,26 @@
 package com.devstoriesafrica.devstoriesafrica.ui.auth.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import com.devstoriesafrica.devstoriesafrica.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
-
+    override fun onStart() {
+        super.onStart()
+        (activity as AppCompatActivity).supportActionBar?.hide()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,4 +29,28 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupUi()
+    }
+
+    private fun setupUi() {
+        initSpannable()
+    }
+    private fun initSpannable() {
+        val spanString = SpannableString("Already have an account? Login")
+        val foregroundSpan = ForegroundColorSpan(
+            ContextCompat.getColor(requireContext(), R.color.yellow))
+        spanString.setSpan(foregroundSpan,
+            25,
+            30,
+            Spanned.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity).supportActionBar?.hide()
+    }
 }
