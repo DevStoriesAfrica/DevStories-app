51541655d5d66a79cc96cf08b7475bce04b259e9
Breens-Thomas <mbakabreens@gmail.com>
2021-10-24 02:48:21 +0300

    Finding reference to MainActivity then setting bottom navigation visibility

--- app/src/main/java/com/devstoriesafrica/devstoriesafrica/ui/base/BaseFragment.kt
+++ app/src/main/java/com/devstoriesafrica/devstoriesafrica/ui/base/BaseFragment.kt
@@ -6,11 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import com.devstoriesafrica.devstoriesafrica.ui.activities.MainActivity
 
 abstract class BaseFragment<out T: ViewBinding> : Fragment() {
     private var _binding: ViewBinding? = null
     protected val binding: T
         get() = _binding as T
+
+    //Fragment will override this and set their own value
+    protected open var bottomNavigationVisibility = View.VISIBLE
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,5 +28,13 @@ abstract class BaseFragment<out T: ViewBinding> : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if(activity is MainActivity) {
+            val mainActivity = activity as MainActivity
+            mainActivity.setBottomNavigationVisibility(bottomNavigationVisibility)
+        }
     }
 }
