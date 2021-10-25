570f92faa31b62996bb757c2c664113d7a11c285
Breens-Thomas <mbakabreens@gmail.com>
2021-10-25 04:00:52 +0300

    Removed navigation from login and signup fragment

--- app/src/main/java/com/devstoriesafrica/devstoriesafrica/ui/auth/fragments/RegisterFragment.kt
+++ app/src/main/java/com/devstoriesafrica/devstoriesafrica/ui/auth/fragments/RegisterFragment.kt
@@ -26,8 +26,6 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>() {
     override val bindingInflater: (LayoutInflater) -> ViewBinding
         get() = FragmentRegisterBinding::inflate
 
-    override var bottomNavigationVisibility = View.GONE
-
     override fun onStart() {
         super.onStart()
         (activity as AppCompatActivity).supportActionBar?.hide()
@@ -119,7 +117,8 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>() {
                         binding.progressBar.visibility = View.VISIBLE
                     }
                     Status.ERROR -> {
-                        Toast.makeText(context,"Error while creating account",Toast.LENGTH_LONG).show()
+                        Toast.makeText(context, "Error while creating account", Toast.LENGTH_LONG)
+                            .show()
                     }
                 }
             }
--- app/src/main/java/com/devstoriesafrica/devstoriesafrica/ui/base/BaseFragment.kt
+++ app/src/main/java/com/devstoriesafrica/devstoriesafrica/ui/base/BaseFragment.kt
@@ -6,15 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
-import com.devstoriesafrica.devstoriesafrica.ui.activities.MainActivity
 
-abstract class BaseFragment<out T: ViewBinding> : Fragment() {
+abstract class BaseFragment<out T : ViewBinding> : Fragment() {
     private var _binding: ViewBinding? = null
     protected val binding: T
         get() = _binding as T
-
-    //Fragment will override this and set their own value
-    protected open var bottomNavigationVisibility = View.VISIBLE
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,25 +20,10 @@ abstract class BaseFragment<out T: ViewBinding> : Fragment() {
         _binding = bindingInflater(inflater)
         return binding.root
     }
+
     protected abstract val bindingInflater: (LayoutInflater) -> ViewBinding
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        if(activity is MainActivity) {
-            val mainActivity = activity as MainActivity
-            mainActivity.setBottomNavigationVisibility(bottomNavigationVisibility)
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        if (activity is MainActivity) {
-            val mainActivity = activity as MainActivity
-            mainActivity.setBottomNavigationVisibility(bottomNavigationVisibility)
-        }
     }
 }
