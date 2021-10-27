3d6c44815ca360b5e4a1b1b4cd2001b8e73ac2ba
Breens-Thomas <mbakabreens@gmail.com>
2021-10-27 15:49:34 +0300

    Setup animated transititons during user navigation

--- app/src/main/java/com/devstoriesafrica/devstoriesafrica/ui/auth/fragments/OtpFragment.kt
+++ app/src/main/java/com/devstoriesafrica/devstoriesafrica/ui/auth/fragments/OtpFragment.kt
@@ -5,12 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
 import com.devstoriesafrica.devstoriesafrica.R
 import com.devstoriesafrica.devstoriesafrica.databinding.FragmentOtpBinding
 import com.devstoriesafrica.devstoriesafrica.ui.base.BaseFragment
+import com.google.android.material.transition.MaterialFadeThrough
 
 class OtpFragment : BaseFragment<FragmentOtpBinding>() {
     override val bindingInflater: (LayoutInflater) -> ViewBinding
         get() = FragmentOtpBinding::inflate
+
+    override fun onStart() {
+        super.onStart()
+        (activity as AppCompatActivity).supportActionBar?.hide()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUI()
+    }
+
+    private fun setUI() {
+        initMaterialTransitions()
+    }
+
+    private fun initMaterialTransitions() {
+        enterTransition = MaterialFadeThrough()
+        exitTransition = MaterialFadeThrough()
+        reenterTransition = MaterialFadeThrough()
+    }
 }
