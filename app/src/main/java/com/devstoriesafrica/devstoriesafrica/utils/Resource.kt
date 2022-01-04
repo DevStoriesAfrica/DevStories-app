b21fd5b68a0d0bc88822929fe9d7dde8a7751738
germainkevinbusiness <67168053+kevingermainbusiness@users.noreply.github.com>
2022-01-04 14:55:56 -0500

    Updated dependencies to latest stable ones

--- app/src/main/java/com/devstoriesafrica/devstoriesafrica/utils/Resource.kt
+++ app/src/main/java/com/devstoriesafrica/devstoriesafrica/utils/Resource.kt
@@ -5,9 +5,11 @@ data class Resource<out T>(val status: Status, val data: T?, val message: String
         fun <T> success(data: T?): Resource<T> {
             return Resource(Status.SUCCESS, data, null)
         }
+
         fun <T> error(msg: String, data: T?): Resource<T> {
             return Resource(Status.ERROR, data, msg)
         }
+
         fun <T> loading(data: T?): Resource<T> {
             return Resource(Status.LOADING, data, null)
         }
